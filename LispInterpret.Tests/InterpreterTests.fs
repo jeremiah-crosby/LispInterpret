b0175747d63a57b2a561f256d6b86debcd8169e3
Jeremiah <jeremiahcrosby@gmail.com>
2019-06-24 07:51:05 -0400

    Start interpreter and add interpreting of int and float

---
+++ LispInterpret.Tests/InterpreterTests.fs
@@ -0,0 +1,23 @@
+﻿namespace Tests
+
+open NUnit.Framework
+open FsUnit
+open Lexer
+open Parser
+open Interpreter
+
+type InterpreterTests () =
+
+    [<Test>]
+    member this.``should interpret int`` () =
+        let tokens = lex "5"
+        let parsed = parse tokens
+        let result = parsed |> List.head |> evalExpression
+        result |> should equal (IntResult(5))
+
+    [<Test>]
+    member this.``should interpret float`` () =
+        let tokens = lex "5.8"
+        let parsed = parse tokens
+        let result = parsed |> List.head |> evalExpression
+        result |> should equal (FloatResult(5.8))
---
+++ LispInterpret/Interpreter.fs
@@ -0,0 +1,18 @@
+﻿module Interpreter
+
+open Syntax
+
+type EvalResult =
+    | IntResult of int
+    | FloatResult of float
+    | Empty
+
+//type Environment = {
+    
+//}
+
+let evalExpression (expr: Expression) =
+    match expr with
+    | IntExpr(n) -> IntResult n
+    | FloatExpr(d) -> FloatResult d
+    | _ -> Empty
