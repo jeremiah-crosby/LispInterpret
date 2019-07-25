346d96c8f78c93ba009113e9c2d5056ffd246eee
Jeremiah <jeremiahcrosby@gmail.com>
2019-07-25 07:18:20 -0400

    Fixed unit tests by implementing custom equality for Expression

--- LispInterpret/Syntax.fs
+++ LispInterpret/Syntax.fs
@@ -11,7 +11,10 @@ type FunctionArgument = {
     Name: string
 }
 
+[<CustomEqualityAttribute>]
+[<NoComparison>]
 type Expression =
+
     | IntExpr of int
     | FloatExpr of float
     | SymbolExpr of name: string
@@ -21,6 +24,22 @@ type Expression =
     | FunctionExpr of FunctionData
     | BoolExpr of bool
     | NilExpr
+    override this.Equals(obj) =
+        match obj with
+        | :? Expression as other ->
+            match this, other with
+            | IntExpr x, IntExpr y -> x = y
+            | FloatExpr f1, FloatExpr f2 -> f1 = f2
+            | SymbolExpr s1, SymbolExpr s2 -> s1 = s2
+            | ErrorExpr e1, ErrorExpr e2 -> e1 = e2
+            | ListExpr ls1, ListExpr ls2 -> ls1 = ls2
+            | StringExpr s1, StringExpr s2 -> s1 = s2
+            | FunctionExpr f1, FunctionExpr f2 ->
+                f1.Name = f2.Name && f1.Arguments = f2.Arguments && f1.Body = f2.Body
+            | BoolExpr b1, BoolExpr b2 -> b1 = b2
+            | NilExpr, NilExpr -> true
+            | _ -> false
+        | _ -> false
 and FunctionData = {
     Name: string
     Arguments: FunctionArgument list
@@ -30,6 +49,6 @@ and FunctionData = {
 and Environment = {
     ParentEnv: Option<Environment>
     Variables: Map<string, Expression ref> ref
-    Intrinsics: Map<string, IntrinsicFunction> ref
+    Intrinsics: Map<string, Expression list -> Environment -> Expression> ref
 }
-and IntrinsicFunction = Expression list -> Environment -> Expression
+
