aeedc9a4f6d20c4c65e9c8d528a61b57e5a3f15d
Jeremiah <jeremiahcrosby@gmail.com>
2019-08-12 22:01:42 -0400

    Implement load for loading source from files

--- LispInterpret/Program.fs
+++ LispInterpret/Program.fs
@@ -5,6 +5,15 @@ open Syntax
 open Parser
 open Interpreter
 
+let load filename env =
+    let reader = System.IO.File.OpenText(filename)
+    let source = reader.ReadToEnd()
+    let tokens = lex source
+    let parsed = parse tokens
+    let result = evalExpressions env parsed
+    printExpression (result, env) |> System.Console.Out.WriteLine
+    reader.Close()
+
 let readExpression (reader: System.IO.TextReader) =
     let mutable input = ""
     while (not (input.EndsWith(';'))) do
@@ -14,19 +23,24 @@ let readExpression (reader: System.IO.TextReader) =
     else
         input.Substring(0, input.Length - 1)
 
-let rec repl env =
+let rec repl env reader =
     try
         printf "\n>> "
-        let output = readExpression (System.Console.In) |> lex |> parse |> evalExpressions env
+        let output = readExpression (reader) |> lex |> parse |> evalExpressions env
         printExpression (output, env) |> System.Console.Out.WriteLine
-        repl env
+        repl env reader
     with ex ->
         printf "Error: %s\n" ex.Message
-        repl env
+        repl env reader
 
 let [<EntryPoint>] main _ =
     let env = createGlobalEnv ()
     env.Intrinsics := env.Intrinsics.Value.Add("quit", (fun _ _ -> System.Environment.Exit(0); NilExpr))
+    env.Intrinsics := env.Intrinsics.Value.Add("load", (fun args env ->
+                                                            match args with
+                                                            | [StringExpr filename] ->
+                                                                load filename env
+                                                                NilExpr))
     System.Console.WriteLine("Welcome to LispInterpret")
-    repl env
+    repl env System.Console.In
     0
